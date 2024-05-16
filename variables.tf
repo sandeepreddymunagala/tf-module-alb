7d3a5ae8324c95265f39e76a33cc9618125b8b0a
sandeep <munagalasandeepreddy@gamail.com>
2024-05-16 10:19:52 +0200

    learn terraform

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "name" {}
+variable "internal" {}
+variable "load_balancer_type" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "sg_subnet_cidr" {}
+variable "subnets" {}
+variable "tags" {}
